279f1343e91204a6edbc5137147d4cf0d2c071d4
Jack Chambers <jtcroxley@gmail.com>
2020-11-13 10:08:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "$"
 const version = "1.0"
@@ -36,14 +36,16 @@ client.on('message', message => {
                 message.channel.send("Invalid poll "+pingUser(message.author));
                 break;
             }
+
             const o1 = args[0];
             const o2 = args[2];
             const e1 = args[1];
             const e2 = args[3];
+
             const promise = pollChannel.send(o1+" "+e1+" or "+o2+" "+e2+" ?");
-            promise.then(function (message) {
-                message.react(e1);
-                message.react(e2);
+            promise.then(function (pollMessage) {
+                pollMessage.react(e1);
+                pollMessage.react(e2);
             });
             break;
         case('channel'):
@@ -58,6 +60,9 @@ client.on('message', message => {
             console.log("Set poll channel to: "+pollChannel.name);
             message.channel.send("Poll channel set to: " + pollChannel + " "+pingUser(message.author));
             break;
+        case('whosafaggot'):
+            message.channel.send('adam kebalka');
+            break;
         case("help"):
             message.channel.send("**Commands:**");
             message.channel.send(" - "+prefix+"create <option1> <emoji1> <option2> <emoji2>");
@@ -67,4 +72,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login("Nzc2NDg3NzA3MTQ4Mjg4MDAw.X61mhw.K5Bkpq6PCpskQsgXuN1IxKz5PC8");
